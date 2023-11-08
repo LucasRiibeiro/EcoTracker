632775c30a79a570ac199ab2f116baab12ca4705
lucas-faria-am <lucas91120053@gmail.com>
2023-11-08 15:33:24 -0300

    Estilização do Clima Tracker

--- src/API/Clima&PoluicaoTracker/styles.ts
+++ src/API/Clima&PoluicaoTracker/styles.ts
@@ -1,54 +1,71 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-
     headerClima: {
-      alignSelf: 'center',
-      color: 'green',
-      fontSize: 20,
+        alignSelf: "center",
+        color: "green",
+        fontSize: 20,
+        fontWeight: "bold",
     },
     headerPoluicao: {
-      alignSelf: 'center',
-      color: 'orange',
-      fontSize: 20,
+        alignSelf: "center",
+        color: "orange",
+        fontSize: 20,
     },
     container: {
-      flex: 1,
-      marginTop: 25,
-      justifyContent: 'center',
-      padding: 10,
-      backgroundColor: 'gray',
+        flex: 1,
+        marginTop: 25,
+        justifyContent: "center",
+        padding: 10,
+        backgroundColor: "#007FBC",
+        borderRadius: 10,
     },
     inputContainer: {
-      alignItems: 'center',
+        alignItems: "center",
+        backgroundColor: "#2A598F",
+        borderRadius: 10,
+        marginBottom: 15,
     },
     heading: {
-      fontSize: 30,
-      fontWeight: 'bold',
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "#fff",
     },
     italicText: {
-      fontStyle: 'italic',
-      marginVertical: 15,
+        fontStyle: "italic",
+        marginVertical: 15,
+        fontSize: 17,
+        color: "#fff",
     },
     picker: {
-      width: 200,
-      marginVertical: 10,
+        width: 200,
+        marginVertical: 10,
+        backgroundColor: "#466F8F",
+        borderRadius: 10,
+        color: "#fff",
     },
     weatherContainer: {
-      marginTop: 20,
+        padding: 5,
+        marginTop: 5,
+        backgroundColor: "#052103",
+        borderRadius: 10,
     },
     pollutionContainer: {
-      marginTop: 20,
+        padding: 5,
+        marginTop: 10,
+        backgroundColor: "#5E4004",
+        borderRadius: 10,
     },
     weatherText: {
-      fontSize: 13,
-      marginVertical: 5,
-      alignSelf: 'center'
+        fontSize: 13,
+        marginVertical: 5,
+        alignSelf: "center",
+        color: "#fff",
     },
     pollutionText: {
-      fontSize: 13,
-      marginVertical: 5,
-      alignSelf: 'center'
+        fontSize: 13,
+        marginVertical: 5,
+        alignSelf: "center",
+        color: "#fff",
     },
-  });
-  
+});
